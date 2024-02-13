4e3414474a1f597c4ff8256622a3e501b76ee610
mdehtisham <mefxe01@gmail.com>
2024-02-13 16:53:15 +0530

    made initial file setup

---
+++ lib/data.ts
@@ -0,0 +1,106 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+// import corpcommentImg from "@/public/corpcomment.png";
+// import rmtdevImg from "@/public/rmtdev.png";
+// import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Gainsight",
+    location: "Hyderabad",
+    description:
+      "Worked as UI dev",
+    icon: React.createElement(LuGraduationCap),
+    date: "May 2022 - Feb 2024",
+  },
+  {
+    title: "Frontend Developer",
+    location: "Bangalore",
+    description:
+      "I worked as a front-end developer.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2021 - 2022",
+  },
+  {
+    title: "Associate Development Engineer",
+    location: "Bangalore",
+    description:
+      "Worked as Angular Developer",
+    icon: React.createElement(FaReact),
+    date: "Feb 2021 - Sept 2021",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    // imageUrl: corpcommentImg,
+    imageUrl: null
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    // imageUrl: rmtdevImg,
+    imageUrl: null
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    // imageUrl: wordanalyticsImg,
+    imageUrl: null
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Angular",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "Express",
+  "Framer Motion",
+] as const;
